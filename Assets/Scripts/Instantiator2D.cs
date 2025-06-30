87c76ccdd8e5d972a46c7ca1aa102bbe5972c718
JohnGhidot <marco.ghidotti2@gmail.com>
2025-06-30 21:24:06 +0200

    Sistemato script Instantiator

--- Assets/Scripts/Instantiator2D.cs
+++ Assets/Scripts/Instantiator2D.cs
@@ -7,8 +7,8 @@ public class Instantiator2D : MonoBehaviour
     [SerializeField] private GameObject _quadPrefab;
     [SerializeField] private int _column = 6;
     [SerializeField] private int _row = 6;
-    [SerializeField] private float _offsetCol = 1f;
-    [SerializeField] private float _offsetRow = 1f;
+    [SerializeField] private float _offsetCol = 1.5f;
+    [SerializeField] private float _offsetRow = 1.5f;
     [SerializeField] private Transform _parentTransform;
 
     // Start is called before the first frame update
@@ -29,7 +29,7 @@ public class Instantiator2D : MonoBehaviour
         {
             for (int x = 0; x < _column; x++)
             {
-                Vector3 pos = new Vector3(x * _offsetCol, y * _offsetRow, 0f);
+                Vector3 pos = new Vector3(x * _offsetCol, -y * _offsetRow, 0f);
                 Instantiate(_quadPrefab, pos, Quaternion.identity, this.transform);
             }
         }
